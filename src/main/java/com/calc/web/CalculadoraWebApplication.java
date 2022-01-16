942cfea2ddac9ca0f78b6e90fdc938ab57c0de98
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-16 19:43:11 -0300

    Project created

---
+++ src/main/java/com/calc/web/CalculadoraWebApplication.java
@@ -0,0 +1,13 @@
+package com.calc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraWebApplication.class, args);
+	}
+
+}
